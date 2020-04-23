24dde78fbb10c59e1ff6746cc16c3ce38b4b85a5
eriosstarks28 <58051709+eriosstarks28@users.noreply.github.com>
2020-04-23 19:30:35 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,24 +8,31 @@ const body = document.querySelector("body");
 
 fetch(profileUrl)
   .then((response) => response.json())
-  .then(getProfiles)
-  .then(generateHTML);
+  .then(generatePerson);
 
-function getProfiles(json) {
-  const profile = json.results;
+function generatePerson(data) {
+  data = data.results;
+  for (i = 0; i < data.length; i++) {
+    createProfile(data);
+    createModal(data);
+  }
 
-  return profile;
+  const cards = document.querySelectorAll(".card");
+  const [...modal] = document.querySelectorAll(".modal-container");
+
+  cards.forEach((card, index) =>
+    card.addEventListener("click", () => {
+      let num = index;
+      modal[index].style.display = "inherit";
+      modalBtn(modal);
+    })
+  );
 }
 
-function generateHTML(data) {
-  for (i = 0; i < data.length; i++) {
-    const card = document.createElement("div");
-    const modal = document.createElement("div");
-    card.className = "card";
-    modal.className = "moda;-container";
-    modal.style.dispaly = "none";
-
-    card.innerHTML = `
+function createProfile(data) {
+  const card = document.createElement("div");
+  card.className = "card";
+  card.innerHTML = `
     <div class="card-img-container">
         <img class="card-img" src="${data[i].picture.large}"alt="profile picture">
     </div>
@@ -35,6 +42,38 @@ function generateHTML(data) {
         <p class="card-text cap">${data[i].location.city}</p>
     </div>
 </div>`;
-    gallery.appendChild(card);
-  }
+  gallery.appendChild(card);
+}
+
+function createModal(data) {
+  const modal = document.createElement("div");
+  modal.className = "modal-container";
+  modal.innerHTML = `
+   <div class="modal">
+       <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+       <div class="modal-info-container">
+           <img class="modal-img" src="${data[i].picture.large}" alt="profile picture">
+           <h3 id="name" class="modal-name cap">${data[i].name.first},${data[i].name.last}</h3>
+           <p class="modal-text">${data[i].email}</p>
+           <p class="modal-text cap">${data[i].location.city}</p>
+           <hr>
+           <p class="modal-text">${data[i].cell}</p>
+           <p class="modal-text">${data[i].location.street},${data[i].location.city},${data[i].location.postcode}</p>
+           <p class="modal-text">Birthday:${data[i].dob.date}</p>
+       </div>
+   </div>`;
+
+  modal.style.display = "none";
+  body.appendChild(modal);
+}
+
+function modalBtn(modal) {
+  console.log(modal);
+  modal.forEach((box) =>
+    box.addEventListener("click", () => {
+      if ((event.target.Tagname = "button")) {
+        box.style.display = "none";
+      }
+    })
+  );
 }
